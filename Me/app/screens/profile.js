6ca4885cedbf5d4a2b8b00157f68d756440e094d
Atul Anand <atul.dc@gmail.com>
2019-06-14 00:19:10 +0530

    2 tabs done
    done

--- Me/app/screens/profile.js
+++ Me/app/screens/profile.js
@@ -2,31 +2,119 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 
-export default class Profile extends Component {
+export default class ProfileView extends Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-            profile 
-        </Text>
+          <View style={styles.header}>
+            <View style={styles.headerContent}>
+                <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar2.png'}}/>
+                <Text style={styles.name}>
+                  Atul
+                </Text>
+            </View>
+          </View>
+
+          <View style={styles.profileDetail}>
+            <View style={styles.detailContent}>
+              <Text style={styles.title}>Photos</Text>
+              <Text style={styles.count}>200</Text>
+            </View>
+            <View style={styles.detailContent}>
+              <Text style={styles.title}>Followers</Text>
+              <Text style={styles.count}>200</Text>
+            </View>
+            <View style={styles.detailContent}>
+              <Text style={styles.title}>Following</Text>
+              <Text style={styles.count}>200</Text>
+            </View>
+          </View>
+
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 1</Text>  
+              </TouchableOpacity> 
+              <Text style={styles.description}>Demo</Text>
+            </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header:{
+    backgroundColor: "#00CED1",
+  },
+  headerContent:{
+    padding:30,
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  profileDetail:{
+    alignSelf: 'center',
+    marginTop:200,
+    alignItems: 'center',
+    flexDirection: 'row',
+    position:'absolute',
+    backgroundColor: "#ffffff"
+  },
+  detailContent:{
+    margin:10,
+    alignItems: 'center'
+  },
+  title:{
+    fontSize:20,
+    color: "#00CED1"
+  },
+  count:{
+    fontSize:18,
+  },
+  bodyContent: {
     flex: 1,
+    alignItems: 'center',
+    padding:30,
+    marginTop:40
+  },
+  textInfo:{
+    fontSize:18,
+    marginTop:20,
+    color: "#696969",
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00CED1",
   },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
+  description:{
+    fontSize:20,
+    color: "#00CED1",
+    marginTop:10,
+    textAlign: 'center'
+  },
 });
